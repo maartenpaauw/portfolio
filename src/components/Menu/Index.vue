b017a0b5fc7f16a5c027867fb8ce86ffa66f8088
Maarten Paauw <maartenpaauw@gmail.com>
2017-12-05 14:54:10 +0100

    chore(menu): burger menu icon is white

--- src/components/Menu/Index.vue
+++ src/components/Menu/Index.vue
@@ -53,7 +53,7 @@
   }
 </script>
 
-<style lang="scss" scoped="scoped">
+<style lang="scss">
   nav {
     &.bg-transparent-white {
       background-color: rgba(255, 255, 255, 0.1);
@@ -62,6 +62,17 @@
     .navbar-brand {
       font-size: 1rem;
       border: solid white 2px;
+    }
+
+    .navbar-toggler {
+      border-color: white !important;
+      border-radius: 0;
+      border-width: 2px;
+      font-size: 1.1rem;
+
+      .navbar-toggler-icon {
+        background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgb(255,255,255)' stroke-width='2' stroke-linecap='square' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E");
+      }
     }
 
     .nav-link {

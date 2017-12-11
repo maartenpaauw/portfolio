0e72c5e6c8022d64f83008afc57b84f38eadac1c
Maarten Paauw <maartenpaauw@gmail.com>
2017-12-11 23:51:42 +0100

    chore(json-resume): some additional information

--- src/resume/index.js
+++ src/resume/index.js
@@ -1,7 +1,9 @@
+import additions from './additions.json'
 import resume from './resume.json'
 import vocational from './vocational.json'
 
 export default {
+  ...additions,
   ...resume,
   ...vocational
 }
---
+++ src/store/modules/additions/getters.js
@@ -0,0 +1,17 @@
+export default {
+  birthday: state => {
+    return state.birthday
+  },
+  gender: state => {
+    return state.gender
+  },
+  nationality: state => {
+    return state.nationality
+  },
+  marital_status: state => {
+    return state.marital_status
+  },
+  drivers_license: state => {
+    return state.drivers_license
+  }
+}

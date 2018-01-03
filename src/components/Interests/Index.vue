31df72f282ba227077f5d8f43e572813e905a6b1
Maarten Paauw <maartenpaauw@gmail.com>
2018-01-03 21:45:25 +0100

    Revert "fix(json-resume): programmeren -> coderen"
    This reverts commit ac5f0d864397aa6539879b1ad2ba1302e953220e.

--- src/components/Interests/Index.vue
+++ src/components/Interests/Index.vue
@@ -44,7 +44,7 @@
           'home-assistant': 'skill skill-home-assistant',
           'koffie': 'fa-coffee',
           'muziek': 'fa-music',
-          'coderen': 'fa-code',
+          'programmeren': 'fa-code',
           'reizen': 'fa-plane'
         }[slugify(name.toLowerCase())]
       }

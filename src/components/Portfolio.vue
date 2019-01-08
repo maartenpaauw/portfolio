065bd0be503c06e17cce09bdc8137d586a8781a9
maartenpaauw <maartenpaauw@gmail.com>
2019-01-08 12:11:43 +0100

    chore(internship): replaced it with placeholder component

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -6,9 +6,9 @@
     portfolio-cv
     portfolio-whatpulse
     portfolio-certificates
-    portfolio-internship(:internship="iternships[0]")
+    portfolio-other
     portfolio-skills
-    portfolio-internship(:internship="iternships[1]")
+    portfolio-other
     portfolio-references
     portfolio-interests
     portfolio-projects
@@ -19,41 +19,27 @@
   import Header from '@/components/Header/Index'
   import Introduction from '@/components/Introduction/Index'
   import StackOverflow from '@/components/StackOverflow/Index'
-  import Internship from '@/components/Internship/Index'
   import CV from '@/components/CV/Index'
   import Whatpulse from '@/components/Whatpulse/Index'
   import Interests from '@/components/Interests/Index'
   import Footer from '@/components/Footer/Index'
   import References from '@/components/References/Index'
+  import Other from '@/components/Other/Index'
   import Projects from '@/components/Projects/Index'
   import Skills from '@/components/Skills/Index'
   import Certificates from '@/components/Certificates/Index'
 
   export default {
     name: 'portfolio',
-    data () {
-      return {
-        iternships: [{
-          type: 'stage',
-          from: new Date('2018-08-01'),
-          to: new Date('2019-01-01')
-        },
-        {
-          type: 'afstudeerstage',
-          from: new Date('2019-02-01'),
-          to: new Date('2019-07-01')
-        }]
-      }
-    },
     components: {
       portfolioHeader: Header,
       portfolioIntroduction: Introduction,
       portfolioStackOverflow: StackOverflow,
-      portfolioInternship: Internship,
       portfolioCv: CV,
       portfolioWhatpulse: Whatpulse,
       portfolioInterests: Interests,
       portfolioReferences: References,
+      portfolioOther: Other,
       portfolioProjects: Projects,
       portfolioSkills: Skills,
       portfolioCertificates: Certificates,
